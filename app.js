eedcd460cccb07ac53a8e185d262ae63c18209f5
kmorel <kmorel@stevens.edu>
2018-02-18 17:26:23 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const todoItems = require("./todo");
+const connection = require("./mongoConnection");
+
+const main = async () => {
+	console.log("Creating Task......");
+	const item = await todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?")
+	console.log(item);
+	console.log("Creating Second Task.....");
+	const item2 = await todoItems.createTask("Play Pokemon with Twitch TV", "Should we revive Helix?");
+	const allItems = await todoItems.getAllTasks();
+	console.log(allItems);
+	console.log("Removing first task.......");
+	await todoItems.removeTask(item._id);
+
+	const getItems = await todoItems.getAllTasks();
+	console.log("These are the remaining task......");
+	console.log(getItems);
+	
+	console.log("Completing remaining task");
+	await todoItems.completeTask(item2._id);
+	const updatedItem2 = await todoItems.getTask(item2._id);
+	console.log(updatedItem2);
+
+	const db = await connection();
+	await db.serverConfig.close();
+};
+
+main().catch(error => {
+	console.log(error);
+});
---
+++ todo.js
@@ -0,0 +1,78 @@
+const mongoCollection = require("./mongoCollection");
+const todoItems = mongoCollection.todoItems;
+const uuidv1 = require("uuid/v1");
+
+let exportedMethods = {
+
+	async getTask(id) {
+		if (!id) throw "You must provide an id to search for";
+
+		const todoItemsCollection = await todoItems();
+		const todo = await todoItemsCollection.findOne({ _id: id });
+		if (todo === null) throw "No item with that id";
+
+		return todo;
+	},
+	async getAllTasks() {
+		const todoItemsCollection = await todoItems();
+
+		const items = await todoItemsCollection.find({}).toArray();
+
+		return items;
+	},
+	async createTask(title, description) {
+		if (!title) throw "You must provide a title for the item";
+		if (!description) throw "You must provide a description for the item";
+
+		const uuid = uuidv1();
+		const todoItemsCollection = await todoItems();
+		let newItem = {
+			_id: uuid,
+			title: title,
+			description: description,
+			completed: false,
+			completedAt: null
+		}
+
+		const insertInfo = await todoItemsCollection.insertOne(newItem);
+		if (insertInfo.insertedCount === 0) throw "Could not add item";
+		
+		const item = await this.getTask(uuid);
+		return item;
+	},
+	async completeTask(taskId) {
+		if (!taskId) throw "You must provide an id for a task to complete";
+	
+		const todoItemsCollection = await todoItems();
+		const itemToUpdate = await this.getTask(taskId);
+		const updatedItem = {
+			_id: itemToUpdate._id,
+			title: itemToUpdate.title,
+			description: itemToUpdate.description,
+			completed: true,
+			completedAt: new Date()
+		}
+
+		const updateInfo = await todoItemsCollection.updateOne({ _id: taskId }, updatedItem);
+		if (updatedInfo.modifiedCount === 0) {
+			throw "Could not complete task successfully";
+		}
+		return await this.getTask(taskId);
+	},
+
+	async removeTask(id) {
+		if (!id) throw "You must provide an id for a task to remove";
+	
+		const todoItemsCollection = await todoItems();
+		const deletionInfo = await todoItemsCollection.removeOve({ _id: id });
+	
+		if (deletionInfo.deletedCount === 0) {
+			throw "Could not delete task with id $(id)";
+		}
+		else {
+			return true;
+		}
+	}
+};
+
+module.exports = exportedMethods;
